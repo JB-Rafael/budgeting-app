b712c7411afcbaa3ee59a36ddd37e621c9db5bca
James Rafael <jamesrafael36@gmail.com>
2026-03-08 13:11:44 +0800

    cors 2

--- demo/src/main/java/com/example/demo/controller/CategoryController.java
+++ demo/src/main/java/com/example/demo/controller/CategoryController.java
@@ -1,10 +1,15 @@
 package com.example.demo.controller;
 
-import org.springframework.web.bind.annotation.*;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
-import com.example.demo.repository.CategoryRepository;
 import com.example.demo.model.Category;
+import com.example.demo.repository.CategoryRepository;
 
 @CrossOrigin(origins = "https://jbrafael-budget-app.vercel.app")
 @RestController
