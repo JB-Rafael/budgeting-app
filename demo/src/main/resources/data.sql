c1d4266b2e5989f99c42dff7a66183d81501428f
James Rafael <jamesrafael36@gmail.com>
2026-03-08 12:05:51 +0800

    connect neon postgres

---
+++ demo/src/main/resources/data.sql
@@ -0,0 +1,5 @@
+INSERT INTO categories (name,type) VALUES ('food','expense');
+INSERT INTO categories (name,type) VALUES ('rent','expense');
+INSERT INTO categories (name,type) VALUES ('transport','expense');
+INSERT INTO categories (name,type) VALUES ('entertainment','expense');
+INSERT INTO categories (name,type) VALUES ('salary','income');
